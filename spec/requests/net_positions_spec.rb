d952db6a6c049ac924f8afec3a1d2796629a7c93
Subhash Chandra <TMaYaD+github@gmail.com>
2011-10-08 18:54:02 +0530

    added inline nested form for fixed income within new net position

--- spec/requests/net_positions_spec.rb
+++ spec/requests/net_positions_spec.rb
@@ -13,11 +13,29 @@ describe "NetPositions" do
       stock # ensure stock is created
       click_on 'Add New Stock'
       select stock.name, :from => "Security"
+
       fill_in "Price", :with => 200
       #fill_in "Date", :with => "29-11-2010"
       fill_in "Quantity", :with => 30
       click_on "Create Net position"
+
       page.should have_content stock.name
+      page.should have_content 200
+      page.should have_content 30
+    end
+
+    it "adds new fixed income position" do
+      click_on 'Add New Fixed income'
+      fill_in 'Name', :with => 'Test Income'
+      fill_in 'Period', :with => 20
+      fill_in 'Rate of interest', :with => 12
+
+      fill_in "Price", :with => 200
+      #fill_in "Date", :with => "29-11-2010"
+      fill_in "Quantity", :with => 30
+      click_on "Create Net position"
+
+      page.should have_content 'Test Income'
       page.should have_content 200
       page.should have_content 30
     end
@@ -27,10 +45,12 @@ describe "NetPositions" do
       fill_in 'Name', :with => 'Test Loan'
       fill_in 'Period', :with => 20
       fill_in 'Rate of interest', :with => 12
+
       fill_in "Price", :with => 200
       #fill_in "Date", :with => "29-11-2010"
       fill_in "Quantity", :with => 30
       click_on "Create Net position"
+
       page.should have_content 'Test Loan'
       page.should have_content 200
       page.should have_content 30
