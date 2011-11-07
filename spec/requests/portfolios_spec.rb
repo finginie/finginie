a97fe62d00bbe1741a455a3f1a3dd6452b4ff142
Subhash Chandra <TMaYaD@LoonyB.in>
2011-11-08 01:31:23 +0530

    [finish #20654063] Grouped net positions by their security type

--- spec/requests/portfolios_spec.rb
+++ spec/requests/portfolios_spec.rb
@@ -1,11 +1,22 @@
 require 'spec_helper'
 
 describe "Portfolios" do
-  describe "GET /portfolios" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get portfolios_path
-      response.status.should be(200)
+  let (:portfolio) { create :portfolio, :user => user }
+  login_user
+
+  it "groups net positions by asset type" do
+    stock_position = create :net_position, :security => create(:stock), :portfolio => portfolio
+    loan_position = create :net_position, :security => create(:loan), :portfolio => portfolio
+    fixed_income_position = create :net_position, :security => create(:fixed_income), :portfolio => portfolio
+    visit portfolio_path portfolio
+    within "section.Stock table" do
+      page.should have_content stock_position.security.name
+    end
+    within "section.FixedIncome table" do
+      page.should have_content fixed_income_position.security.name
+    end
+    within "section.Loan table" do
+      page.should have_content loan_position.security.name
     end
   end
 end
