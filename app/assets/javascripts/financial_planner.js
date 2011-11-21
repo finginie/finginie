963bfc86c31a78106a6512fd30b4b2311a142f78
Subhash Chandra <TMaYaD@LoonyB.in>
2011-11-21 19:56:20 +0530

    added checks to the financial planner javascript to workaround errors

--- app/assets/javascripts/financial_planner.js
+++ app/assets/javascripts/financial_planner.js
@@ -3,116 +3,118 @@
 // You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
 
 $(document).ready(function() {
-  new Highcharts.Chart({
-    chart: {
-      renderTo: 'asset-allocation-chart'
-    },
-    title: {
-      text: 'Suggested Asset Allocation'
-    },
-    series: [{
-      type: 'pie',
-      data: JSON.parse($("#asset-allocation-chart").attr("data-asset-allocation"))
-    }],
-    legend: {
-      labelFormatter: function() {
-        return this.name + ':  ' + this.y + '%';
+  if ($("#asset-allocation-chart").attr("data-asset-allocation") != undefined) {
+    new Highcharts.Chart({
+      chart: {
+        renderTo: 'asset-allocation-chart'
+      },
+      title: {
+        text: 'Suggested Asset Allocation'
+      },
+      series: [{
+        type: 'pie',
+        data: JSON.parse($("#asset-allocation-chart").attr("data-asset-allocation"))
+      }],
+      legend: {
+        labelFormatter: function() {
+          return this.name + ':  ' + this.y + '%';
+        }
+      },
+      tooltip: {
+        formatter: function() {
+          return '' + this.point.name +': '+ (Math.round(this.y*10) / 10) + '%';
+          }
+      },
+      plotOptions: {
+        pie: {
+          allowPointSelect: true,
+          cursor: 'pointer',
+          dataLabels: {
+            enabled: false
+                      },
+          showInLegend: true
+             }
       }
-    },
-    tooltip: {
-      formatter: function() {
-        return '' + this.point.name +': '+ (Math.round(this.y*10) / 10) + '%';
-        }
-    },
-    plotOptions: {
-      pie: {
-        allowPointSelect: true,
-        cursor: 'pointer',
-        dataLabels: {
-          enabled: false
-                    },
-        showInLegend: true
-           }
-    }
-  });
-});
-
-$(document).ready(function() {
-  chart1= new Highcharts.Chart({
-    chart: {
-      renderTo: 'emotional-risk-appetite',
-      defaultSeriesType: 'column',
-      inverted: true
-    },
-    title: {
-      text: 'Emotional Risk Appetite'
-    },
-    xAxis: {
-      categories: ['Average','Your Score','Maximum']
-    },
-    yAxis: {
-      tickInterval: 1
-    },
-    tooltip: {
-      formatter: function() {
-        return '' + this.x + ': ' + this.y + '';
-        }
-    },
-    plotOptions: {
-      series: {
-        colorByPoint: true,
-        showInLegend: false
-              },
-      column: {
-        groupPadding : 0.1,
-        pointPadding: 0
-              }
-    },
-    credits: {
-      enabled: false
-    },
-    series: [{
-      data:  JSON.parse($("#emotional-risk-appetite").attr("emotional-risk-appetite-data"))
-            }]
     });
-});
+  };
 
-$(document).ready(function() {
-  chart1= new Highcharts.Chart({
-    chart: {
-      renderTo: 'financial-risk-appetite',
-      defaultSeriesType: 'column',
-      inverted: true
-    },
-    title: {
-      text: 'Financial Risk Appetite'
-    },
-    xAxis: {
-      categories: ['Average','Your Score','Maximum']
-    },
-    yAxis: {
-      tickInterval: 1
-    },
-    tooltip: {
-      formatter: function() {
-        return '' + this.x + ': ' + this.y + '';
-        }
-    },
-    plotOptions: {
-      series: {
-        colorByPoint: true,
-        showInLegend: false
-              },
-      column: {
-        groupPadding : 0.1,
-        pointPadding: 0
-              }
-    },
-    credits: {
-      enabled: false
-    },
-    series: [{
-      data:  JSON.parse($("#financial-risk-appetite").attr("financial-risk-appetite-data"))
-            }]
+  if ($("#emotional-risk-appetite").attr("emotional-risk-appetite-data") != undefined) {
+    new Highcharts.Chart({
+      chart: {
+        renderTo: 'emotional-risk-appetite',
+        defaultSeriesType: 'column',
+        inverted: true
+      },
+      title: {
+        text: 'Emotional Risk Appetite'
+      },
+      xAxis: {
+        categories: ['Average','Your Score','Maximum']
+      },
+      yAxis: {
+        tickInterval: 1
+      },
+      tooltip: {
+        formatter: function() {
+          return '' + this.x + ': ' + this.y + '';
+          }
+      },
+      plotOptions: {
+        series: {
+          colorByPoint: true,
+          showInLegend: false
+                },
+        column: {
+          groupPadding : 0.1,
+          pointPadding: 0
+                }
+      },
+      credits: {
+        enabled: false
+      },
+      series: [{
+        data:  JSON.parse($("#emotional-risk-appetite").attr("emotional-risk-appetite-data"))
+      }]
     });
+  };
+
+  if ($("#financial-risk-appetite").attr("financial-risk-appetite-data") != undefined) {
+    new Highcharts.Chart({
+      chart: {
+        renderTo: 'financial-risk-appetite',
+        defaultSeriesType: 'column',
+        inverted: true
+      },
+      title: {
+        text: 'Financial Risk Appetite'
+      },
+      xAxis: {
+        categories: ['Average','Your Score','Maximum']
+      },
+      yAxis: {
+        tickInterval: 1
+      },
+      tooltip: {
+        formatter: function() {
+          return '' + this.x + ': ' + this.y + '';
+          }
+      },
+      plotOptions: {
+        series: {
+          colorByPoint: true,
+          showInLegend: false
+                },
+        column: {
+          groupPadding : 0.1,
+          pointPadding: 0
+                }
+      },
+      credits: {
+        enabled: false
+      },
+      series: [{
+        data:  JSON.parse($("#financial-risk-appetite").attr("financial-risk-appetite-data"))
+      }]
+    });
+  };
 });
