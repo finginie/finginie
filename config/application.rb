a56ef2f5c2818d5759f2abbb666a62b23bb02663
Subhash Chandra <subhash@investopresto.com>
2012-02-02 19:41:44 +0530

    fixed issue with asset precompilation on heroku

--- config/application.rb
+++ config/application.rb
@@ -73,5 +73,9 @@ module Finginie
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Do not initialize when compiling assets
+    config.assets.initialize_on_precompile = false
+
   end
 end
