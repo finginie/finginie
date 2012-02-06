023218ceb65418a0fc3a72789ae1b722366667ab
anitha-t <ani.iitm@gmail.com>
2012-02-06 12:52:55 +0530

    Added Model News

---
+++ app/models/news.rb
@@ -0,0 +1,14 @@
+class News
+  include Mongoid::Document
+
+  field :company_code, :type => Float
+  field :news_date, :type => Date
+  field :headlines
+  field :source_name
+  field :news_type_description
+  field :news_classfication
+  field :notes
+  field :modify_on, :type => DateTime
+
+  key :company_code, :news_date, :headlines
+end
