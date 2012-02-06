c9b063da124978ea0616ee6527db24baebb5ae6c
anitha-t <ani.iitm@gmail.com>
2012-02-06 14:05:17 +0530

    Added Model Split

---
+++ app/models/split.rb
@@ -0,0 +1,14 @@
+class Split
+  include Mongoid::Document
+
+  field :company_code, :type => Float
+  field :date_of_announcement, :type => Date
+  field :old_face_value, :type => BigDecimal
+  field :new_face_value, :type => BigDecimal
+  field :record_date, :type => Date
+  field :book_closure_start_date, :type => Date
+  field :book_closure_end_date, :type => Date
+  field :xs_date, :type => Date
+  field :modified_date, :type => DateTime
+  field :delete_flag
+end
