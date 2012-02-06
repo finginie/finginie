df2366cba3e273418e0f3445e195ebcf1144438d
anitha-t <ani.iitm@gmail.com>
2012-02-06 13:10:06 +0530

    Added Model Subsidiary

---
+++ app/models/subsidiary.rb
@@ -0,0 +1,12 @@
+class Subsidiary
+  include Mongoid::Document
+
+  field :company_code, :type => Float
+  field :company_name
+  field :parent_company_code, :type => Float
+  field :parent_company_name
+  field :modified_date, :type => DateTime
+  field :deleteflag
+
+  key :company_code, :parent_company_code
+end
