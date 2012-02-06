f84a04846e0aac539f0811d1e5e0d3ae9a8318b3
anitha-t <ani.iitm@gmail.com>
2012-02-06 12:29:47 +0530

    Added Model CorpGoverancereport

---
+++ app/models/corp_governancereport.rb
@@ -0,0 +1,11 @@
+class CorpGovernancereport
+  include Mongoid::Document
+
+  field :company_code, :type => Float
+  field :year_ending, :type => Date
+  field :notes
+  field :modified_date, :type => DateTime
+  field :deleteflag
+
+  key :company_code, :year_ending
+end
