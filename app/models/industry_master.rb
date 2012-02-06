baefffea9ed26a4c7b6798e5fdd3717084959de1
anitha-t <ani.iitm@gmail.com>
2012-02-06 12:34:17 +0530

    Added Model IndustryMaster

---
+++ app/models/industry_master.rb
@@ -0,0 +1,14 @@
+class IndustryMaster
+  include Mongoid::Document
+
+  field :industry_code, :type => Integer
+  field :industry_name
+  field :broad_industry_code, :type => Integer
+  field :broad_industry_name
+  field :major_sector_code, :type => Integer
+  field :major_sectorName
+  field :modified_date, :type => DateTime
+  field :delete_flag
+
+  key :industry_code
+end
