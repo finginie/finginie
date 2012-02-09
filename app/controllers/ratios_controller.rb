8d8065336a1f191a37f84dd6d3d506d8ee15ec3b
anitha-t <ani.iitm@gmail.com>
2012-02-09 12:32:50 +0530

    Added views for Ratio non-banking for Stock

--- app/controllers/ratios_controller.rb
+++ app/controllers/ratios_controller.rb
@@ -5,5 +5,9 @@ class RatiosController < InheritedResources::Base
       @ratio = BankingRatio.all( conditions: { company_code: @stock.company_code }, sort: [[ :year_ending, :desc ]] ).first
       @ratio = BankingRatioDecorator.decorate(@ratio)
     end
+    if @stock.company && @stock.company.major_sector != 2
+      @ratio = Ratio.all( conditions: { company_code: @stock.company_code }, sort: [[ :year_ending, :desc ]] ).first
+      @ratio = RatioDecorator.decorate(@ratio)
+    end
   end
 end
--- spec/requests/ratios_spec.rb
+++ spec/requests/ratios_spec.rb
@@ -11,6 +11,15 @@ describe "Ratios", :mongoid do
                                             :borrowings_within_india_as_a_per_to_total_borrowings => "83.25",
                                             :deposits_outside_india_as_per_to_total_deposits => "0.22",
                                             :deposits_per_branch => "221.4549"
+    @ratio = create :ratio, :company_code => @company.company_code, :year_ending => '31/03/2011',
+                                            :sell_distribut_cost_comp => "24.23",
+                                            :interest_coverage => "3.2306",
+                                            :total_debtto_ownersfund => "9.6534",
+                                            :investments_turn_ratio => "16.1172",
+                                            :earning_retention_ratio => "100",
+                                            :reported_cash_eps => "12.9583",
+                                            :net_profit_margin => "9.19",
+                                            :asset_turnover_ratio => "2.1852"
 
   end
 
@@ -23,4 +32,18 @@ describe "Ratios", :mongoid do
     page.should have_content "83.25"
   end
 
+  it "should show the ratios page for stock for non-banking" do
+    @company.update_attribute( :major_sector, 1)
+    visit stock_ratios_path(:stock_id => stock.id)
+    page.should have_content 'Capital Adequacy Ratio'
+    page.should have_content 'COMPONENT RATIOS'
+    page.should have_content @ratio.sell_distribut_cost_comp
+    page.should have_content @ratio.interest_coverage
+    page.should have_content @ratio.total_debtto_ownersfund
+    page.should have_content @ratio.investments_turn_ratio
+    page.should have_content @ratio.earning_retention_ratio
+    page.should have_content @ratio.reported_cash_eps
+    page.should have_content @ratio.net_profit_margin
+    page.should have_content @ratio.asset_turnover_ratio
+  end
 end
