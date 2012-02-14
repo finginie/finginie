b4b535d2b17cfc26c4b4ac9d9c152194c1b58f47
Subhash Chandra <subhash@investopresto.com>
2012-02-14 19:38:09 +0530

    fixed error with mass assignment of stock attributes

--- spec/models/stock_spec.rb
+++ spec/models/stock_spec.rb
@@ -7,6 +7,21 @@ describe Stock do
   it { should validate_uniqueness_of :name }
   it { should validate_uniqueness_of :symbol }
 
+  it "should mass assign attributes" do
+    Stock.find_or_initialize_by_id(1).update_attributes({
+      :name                      => "3i Infotech Ltd",
+      :symbol                    => "3IINFOTECH",
+      :sector                    => "Computers - Software - Secondary",
+      :beta                      => "0.86",
+      :eps                       => "6.44",
+      :pe                        => "4.22",
+      :fifty_two_week_high_price => "72.15",
+      :fifty_two_week_high_date  => "11-Nov-10",
+      :fifty_two_week_low_price  => "25.25",
+      :fifty_two_week_low_date   => "26-Aug-11"
+    })
+  end
+
   describe "with scrip", :redis do
     before :each do
       create :scrip, :id => stock.symbol, :last_traded_price => 24, :percent_change => 50
