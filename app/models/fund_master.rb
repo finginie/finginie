9a4f1dc3871ca1a3f6ce3d05e6a5eefc24490f2e
anitha-t <ani.iitm@gmail.com>
2012-02-15 13:39:58 +0530

    Added model FundMaster

---
+++ app/models/fund_master.rb
@@ -0,0 +1,25 @@
+class FundMaster
+  include Mongoid::Document
+
+  field :company_code, :type => BigDecimal
+  field :company_name
+  field :sponser_name
+  field :trustee_company_name
+  field :mf_set_up_date, :type => Date
+  field :amc_code, :type => BigDecimal
+  field :amc_name
+  field :amc_inc_date, :type => Date
+  field :ceo_name
+  field :cio_name
+  field :fund_manager_name
+  field :compliance_officer_name
+  field :inv_service_officer_name
+  field :auditors_name
+  field :custodian_name
+  field :registrar_name
+  field :type_of_mutual_fund
+  field :modified_date, :type => DateTime
+  field :delete_flag
+
+  key :company_code
+end
