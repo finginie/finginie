cd683fe270ce0d58f8b7ac1d81da1dbef5e7c60a
Subhash Chandra <subhash@investopresto.com>
2012-02-22 18:03:18 +0530

    refactored audited results to coerce attributes to decimals

--- app/models/audited_result.rb
+++ app/models/audited_result.rb
@@ -125,67 +125,78 @@ class AuditedResult
 
     key :companycode, :year_ending
 
+    [
+      :equity_capital, :share_appl_money, :pref_capital, :resand_surplus, :revaluation_reserve,
+      :long_term_loan, :unsecured_term_loans, :unsecured_loans, :borrowings_by_bank, :cash_credits,
+      :bills_purchased, :term_loans, :net_block, :capital_wip, :net_current_assets, :misc_exp_not_w_off,
+      :investments, :cash_credits, :bills_purchased, :term_loans, :cash_and_bank_balance,
+      :money_at_call_short_notice, :raw_inventory, :wip_inventory, :finished_goods_inventory,
+      :other_inventory, :excise, :operating_income, :other_recurring_income, :non_recurring_income,
+      :adjusted_pbdit, :financial_expences, :extr_ordinary_items, :layoffretrench_vrs, :insuranceclaims,
+      :reported_net_profit, :numberof_equity_shares
+    ].each do |attr|                                          ##
+      define_method "#{attr}!" do                             # def attr!
+        send(attr) || 0.0                                     #   attr || 0.0
+      end                                                     # end
+    end                                                       ##
+
   def banking_company?
     @company ||= CompanyMaster.where( company_code: companycode).first
-    return true if @company.major_sector == 2
-    return false
+    @company.major_sector == 2
   end
   def net_worth
-    if !@net_worth
-      @net_worth = ( total_share_capital || 0.0 ) + ( resand_surplus || 0.0 ) if (total_share_capital || resand_surplus)
-      @net_worth = ( revaluation_reserve || 0.0 ) + ( total_share_capital || 0.0 ) + ( resand_surplus || 0.0 ) if !banking_company?
-    end
+    @net_worth ||= total_share_capital + resand_surplus!
+    @net_worth += revaluation_reserve! unless banking_company?
     @net_worth
   end
 
   def total_share_capital
-    @total_share_capital ||= ( (equity_capital || 0.0 )+ ( share_appl_money || 0.0) + ( pref_capital || 0.0 ) if equity_capital || share_appl_money || pref_capital )
+    @total_share_capital ||= equity_capital!+ share_appl_money! + pref_capital!
   end
 
   def total_debt
-    (long_term_loan || 0.0 ) + ( unsecured_term_loans || 0.0 ) if !banking_company?
+    long_term_loan! + unsecured_term_loans! unless banking_company?
   end
 
   def total_liabilities
-    return ( net_worth || 0.0 ) + ( total_debt || 0.0 ) if !banking_company?
-    ( net_worth || 0.0 ) + ( unsecured_loans || 0.0 ) + ( borrowings_by_bank || 0.0 )
+    net_worth + ( banking_company? ? unsecured_loans! + borrowings_by_bank! : total_debt )
   end
 
   def advances
-    ( (cash_credits || 0.0) + (bills_purchased || 0.0)  + (term_loans || 0.0)) if banking_company?
+    (cash_credits! + bills_purchased!  + term_loans!) if banking_company?
   end
 
   def total_assets
-    value = (net_block || 0.0) + (capital_wip || 0.0) + (net_current_assets || 0.0) + (misc_exp_not_w_off || 0.0) + (investments || 0.0)
-    value += (advances || 0.0) + (cash_and_bank_balance || 0.0) + (money_at_call_short_notice || 0.0) if banking_company?
+    value = net_block! + capital_wip! + net_current_assets! + misc_exp_not_w_off! + investments!
+    value += advances + cash_and_bank_balance! + money_at_call_short_notice! if banking_company?
     value
   end
 
   def inventory
-    ( raw_inventory || 0.0 ) + ( wip_inventory || 0.0 ) + (finished_goods_inventory || 0.0 ) + ( other_inventory || 0.0 )
+    raw_inventory! + wip_inventory! + finished_goods_inventory! + other_inventory!
   end
 
   def sales
-    ( operating_income || 0.0 )+ ( excise || 0.0 ) if !banking_company?
+    operating_income!+ excise! unless banking_company?
   end
 
   def other_income
-    ( other_recurring_income || 0.0 ) + ( non_recurring_income || 0.0 ) if !banking_company?
+    other_recurring_income! + non_recurring_income! unless banking_company?
   end
 
   def total_income
-    ( operating_income || 0.0 )+ ( other_recurring_income || 0.0 )+ ( non_recurring_income || 0.0 ) if !banking_company?
+    operating_income!+ other_recurring_income!+ non_recurring_income! unless banking_company?
   end
 
   def pbdt
-    ( adjusted_pbdit || 0.0 ) - ( financial_expences || 0.0 ) if !banking_company?
+    adjusted_pbdit! - financial_expences! unless banking_company?
   end
 
   def extra_ordinary_items
-    ( extr_ordinary_items || 0.0 ) + ( layoffretrench_vrs || 0.0 ) + ( insuranceclaims || 0.0 ) if !banking_company?
+    extr_ordinary_items! + layoffretrench_vrs! + insuranceclaims! unless banking_company?
   end
 
   def earnings_per_share
-    return (reported_net_profit / numberof_equity_shares).round(2) if reported_net_profit && numberof_equity_shares
+    (reported_net_profit / numberof_equity_shares).round(2) if reported_net_profit && numberof_equity_shares
   end
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -4,99 +4,22 @@ class Stock < Security
   scope :by_percent_change, lambda { |min, max| where(:symbol => Scrip.find_ids_by_percent_change(min, max)) }
 
   SECTOR_NAMES = [
-    'Automobiles',
-    'Banks - Private Sector',
-    'Banks - Public Sector',
-    'Bearings',
-    'Biotechnology',
-    'Breweries',
-    'Cables',
-    'Carbon Black',
-    'Castings',
-    'Cement',
-    'Ceramics',
-    'Chemicals',
-    'Tobacco',
-    'Chlor-Alkalies',
-    'Cigarettes',
-    'Coffee',
-    'Coke',
-    'Compressors / Drilling Equipment',
-    'Computers',
-    'Construction',
-    'Consumer Durables',
-    'Couriers & Logistics services',
-    'Cycles and Spares',
-    'Decorative - Wood-based',
-    'Detergents / Intermediates',
-    'Distilleries',
-    'Diversified',
-    'Dry Cells',
-    'Dyes',
-    'Edible oils',
-    'Electrical Equipment',
-    'Electrodes',
-    'Electronics',
-    'Engineering',
-    'Engines',
-    'Entertainment',
-    'Fasteners',
-    'Ferro Alloys',
-    'Fertilizers',
-    'Finance',
-    'Fire - Protection Equipments',
-    'Floriculture / Tissue Culture',
-    'Food',
-    'Forgings',
-    'Gas Distribution',
-    'Gems & Jewellery',
-    'Glass',
-    'Granite & Marbles',
-    'Hatcheries',
-    'Hospitals / Allied Medical Services',
-    'Hotels',
-    'Hydraulics',
-    'Industrial Explosives',
-    'Industrial Gas',
-    'IT Enabled Services',
-    'Laminates',
-    'Leather',
-    'Lubricants',
-    'Machine Tools',
-    'Metal',
-    'Mining',
-    'Miscellaneous',
-    'Plastics',
-    'Oil',
-    'Packaging',
-    'Paints',
-    'Paper',
-    'Personal Care',
-    'Pesticides',
-    'Petrochemicals',
-    'Pharmaceuticals',
-    'Photographics',
-    'Power',
-    'Printing & Stationery',
-    'Pumps',
-    'Refineries',
-    'Refractories / Intermediates',
-    'Retailing',
-    'Rubber',
-    'Ship',
-    'Solvent Extraction',
-    'Steel',
-    'Sugar',
-    'Tea',
-    'Telecom',
-    'Textile',
-    'Trading Services ( Securities/Commodities)',
-    'Trading',
-    'Transmission Line Towers / Equipment',
-    'Airlines',
-    'Transport',
-    'Travel Agencies',
-    'Tyres',
+    'Automobiles', 'Banks - Private Sector', 'Banks - Public Sector', 'Bearings', 'Biotechnology',
+    'Breweries', 'Cables', 'Carbon Black', 'Castings', 'Cement', 'Ceramics', 'Chemicals', 'Tobacco',
+    'Chlor-Alkalies', 'Cigarettes', 'Coffee', 'Coke', 'Compressors / Drilling Equipment', 'Computers',
+    'Construction', 'Consumer Durables', 'Couriers & Logistics services', 'Cycles and Spares',
+    'Decorative - Wood-based', 'Detergents / Intermediates', 'Distilleries', 'Diversified', 'Dry Cells',
+    'Dyes', 'Edible oils', 'Electrical Equipment', 'Electrodes', 'Electronics', 'Engineering', 'Engines',
+    'Entertainment', 'Fasteners', 'Ferro Alloys', 'Fertilizers', 'Finance', 'Fire - Protection Equipments',
+    'Floriculture / Tissue Culture', 'Food', 'Forgings', 'Gas Distribution', 'Gems & Jewellery', 'Glass',
+    'Granite & Marbles', 'Hatcheries', 'Hospitals / Allied Medical Services', 'Hotels', 'Hydraulics',
+    'Industrial Explosives', 'Industrial Gas', 'IT Enabled Services', 'Laminates', 'Leather', 'Lubricants',
+    'Machine Tools', 'Metal', 'Mining', 'Miscellaneous', 'Plastics', 'Oil', 'Packaging', 'Paints', 'Paper',
+    'Personal Care', 'Pesticides', 'Petrochemicals', 'Pharmaceuticals', 'Photographics', 'Power',
+    'Printing & Stationery', 'Pumps', 'Refineries', 'Refractories / Intermediates', 'Retailing', 'Rubber',
+    'Ship', 'Solvent Extraction', 'Steel', 'Sugar', 'Tea', 'Telecom', 'Textile',
+    'Trading Services ( Securities/Commodities)', 'Trading', 'Transmission Line Towers / Equipment',
+    'Airlines', 'Transport', 'Travel Agencies', 'Tyres'
   ]
 
   validates :name, :uniqueness => true
@@ -104,6 +27,7 @@ class Stock < Security
 
   SCRIP_METHODS = [:last_traded_price, :percent_change, :net_change, :volume, :high_price, :low_price, :best_buy_price, :best_buy_quantity, :best_sell_price, :best_sell_quantity]
   delegate *SCRIP_METHODS, :to => :scrip, :allow_nil => true
+  delegate :company_code, :to => :company
 
   def scrip
     Scrip.find(symbol)
@@ -116,10 +40,6 @@ class Stock < Security
   def as_json(options = {})
     options ||= {}
     super(options.merge(:methods => SCRIP_METHODS))
-  end
-
-  def company_code
-    company.company_code if company
   end
 
   def company
