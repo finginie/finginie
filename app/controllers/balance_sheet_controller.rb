de626beca6071bf18064230c51494328f0df5b23
anitha-t <ani.iitm@gmail.com>
2012-02-22 18:29:45 +0530

    Nested singleton resources balance_sheet, cash_flow, profit_loss under stock

--- app/controllers/balance_sheet_controller.rb
+++ app/controllers/balance_sheet_controller.rb
@@ -1,10 +1,10 @@
 class BalanceSheetController < InheritedResources::Base
+  belongs_to :stock, :singleton => true
   actions :show
 
-  def show
-    @stock = Stock.find(params[:stock_id])
-    if @stock.company_code
-      @audited_results = AuditedResult.all(conditions: { companycode: @stock.company_code }, sort: [[ :year_ending, :desc ]], limit: 5)
+  def resource
+    if parent.company_code
+      @audited_results = AuditedResult.all(conditions: { companycode: parent.company_code }, sort: [[ :year_ending, :desc ]], limit: 5)
       @audited_results = AuditedResultDecorator.decorate(@audited_results)
     end
   end
--- app/controllers/cash_flow_controller.rb
+++ app/controllers/cash_flow_controller.rb
@@ -1,10 +1,10 @@
 class CashFlowController < InheritedResources::Base
+  belongs_to :stock, :singleton => true
   actions :show
 
-  def show
-    @stock = Stock.find(params[:stock_id])
-    if @stock.company_code
-      @cash_flows = CashFlow.all(conditions: { company_code: @stock.company_code }, sort: [[ :year_ending, :desc ]], limit: 5)
+  def resource
+    if parent.company_code
+      @cash_flows = CashFlow.all(conditions: { company_code: parent.company_code }, sort: [[ :year_ending, :desc ]], limit: 5)
       @cash_flows = CashFlowCollectionDecorator.new(@cash_flows)
     end
   end
