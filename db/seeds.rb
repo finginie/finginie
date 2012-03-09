aff14b8f2c1deeddcb82f6feb015b82cc659a8a5
anitha-t <ani.iitm@gmail.com>
2012-03-09 18:18:24 +0530

    fixed gold db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,7 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
-Gold.create :name => "Gold",
-            :current_price => 2806.0
+Gold.find_or_create_by_name("gold").update_attributes(:current_price => 2806.0)
 
 Quiz.create :name => "Investing Behaviour Profile",
             :weight => 0,
