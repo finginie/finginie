705adc797b6f38b473a9351b49ea4f7f2770ae27
Subhash Chandra <subhash@investopresto.com>
2012-03-22 15:08:14 +0530

    Removed transaction deletion from migration due to errors
    TODO: do the same manually

--- db/migrate/20120321113420_add_rate_of_redemption_to_fixed_deposit.rb
+++ db/migrate/20120321113420_add_rate_of_redemption_to_fixed_deposit.rb
@@ -1,12 +1,5 @@
 class AddRateOfRedemptionToFixedDeposit < ActiveRecord::Migration
-  class FixedDepositTransaction < ActiveRecord::Base
-  end
-  class FixedDeposit < ActiveRecord::Base
-  end
-
   def change
     add_column :securities, :rate_of_redemption, :decimal
-
-    FixedDepositTransaction.all.each { |t| FixedDeposit.find(t.fixed_deposit_id).destroy && t.destroy if t.action == "sell" } #delete the wrongly associated data
   end
 end
