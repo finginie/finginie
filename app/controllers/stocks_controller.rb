b0af986e510193756a90c18aa9a3a7e5818e43b0
Sankar <esankaranarayanan@gmail.com>
2012-03-29 12:33:23 +0530

    #26639635 Changed live update to auto complete in stock page Added search bar in every stock page.

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -2,12 +2,17 @@ class StocksController < InheritedResources::Base
   actions :index, :show
 
   def resource
+    @search = end_of_association_chain.search
     @chart_url =  "http://www.cs4w.in/Forska/TechnicalChart.aspx?Code=" + super.symbol + "&width=800" + "&height=600"
     @stock = StockDecorator.decorate(super)
   end
 
   def collection
     @search = end_of_association_chain.search(params[:search])
-    @stocks = @search.page params[:page]
+    if params[:search] && !params[:search][:sector_contains]
+      @search.all.map{ |stock| {:value => stock.name, :id => stock.id}}
+    else
+      @stocks = @search.page params[:page]
+    end
   end
 end
--- spec/requests/stocks_spec.rb
+++ spec/requests/stocks_spec.rb
@@ -9,4 +9,26 @@ describe "Stocks" do
     visit stock_path stock
     page.should have_content 24.22
   end
+
+  it "should autocomplete stock name when user fill stock name", :js => true do
+    scrip.save
+    visit stocks_path
+
+    page.execute_script %Q{ $('#search_name_contains').val("#{stock.name[0..5]}").keydown(); }
+    wait_until {  page.should have_selector(".ui-menu-item a:contains('#{stock.name}')") }
+
+    page.execute_script %Q{ $('.ui-menu-item a:contains("#{stock.name}")').trigger('mouseenter').click(); }
+    page.current_path.should eq stock_path stock
+  end
+
+  it "should have search in the stock page", :js => true do
+    scrip.save
+    visit stock_path stock
+
+    page.execute_script %Q{ $('#search_name_contains').val("#{stock.name[0..5]}").keydown(); }
+    wait_until {  page.should have_selector(".ui-menu-item a:contains('#{stock.name}')") }
+
+    page.execute_script %Q{ $('.ui-menu-item a:contains("#{stock.name}")').trigger('mouseenter').click(); }
+    page.current_path.should eq stock_path stock
+  end
 end
