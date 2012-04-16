bde58caf65e9516b04e09727664de45807350ea4
Sankar <esankaranarayanan@gmail.com>
2012-04-16 13:03:21 +0530

    Fixed error undefined method `model' for nil:NilClass in ratios show page

--- spec/requests/ratios_spec.rb
+++ spec/requests/ratios_spec.rb
@@ -53,4 +53,9 @@ describe "Ratios", :mongoid do
     click_link "Ratios"
     page.should have_selector("#new_company")
   end
+
+  it "show page shouldn't throw any error when there is no company exists" do
+    visit stock_ratios_path 18
+    page.status_code.should eq 200
+  end
 end
