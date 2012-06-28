792ed88a534d71497ce75d90deff1698d854f1b8
Subhash Chandra <subhash@investopresto.com>
2012-06-28 17:18:16 +0530

    return a page not found when the blog page does not exist can not be fetched

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,5 +1,7 @@
 class BlogController < ApplicationController
   def show
     render :text => Net::HTTP.get(URI("#{ENV['BLOG_URI']}/#{params[:path]}")).force_encoding("UTF-8"), :layout => true
+  rescue
+    render :status => 404
   end
 end
