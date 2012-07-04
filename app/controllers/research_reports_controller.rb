a16c603b4b836b1b20b9e9fab620e0f0e1d97748
anitha-t <ani.iitm@gmail.com>
2012-07-04 19:08:56 +0530

    Fixed UX and UI issues for Research Reports

--- app/controllers/research_reports_controller.rb
+++ app/controllers/research_reports_controller.rb
@@ -1,9 +1,11 @@
 class ResearchReportsController < InheritedResources::Base
-  optional_belongs_to :stock, :parent_class => DataProvider::Company, :finder => :find_by_slug
+  optional_belongs_to :stock, :parent_class => DataProvider::Company, :finder => :find_by_slug, :instance_name => 'company'
 
   def collection
     conditions = { :query => params[:query] }
     conditions.merge!({ :nse_code => parent.nse_code, :bse_code => parent.bse_code1 }) if parent
-    ResearchReport.filter(conditions)
+    @reports = ResearchReport.filter(conditions)
+    flash.now[:result] = @reports.empty? ? t("empty_search") : (t("results_for_search", :term => params[:query]) if params[:query])
+    @reports
   end
 end
--- spec/requests/research_reports_spec.rb
+++ spec/requests/research_reports_spec.rb
@@ -2,6 +2,21 @@ require 'spec_helper'
 
 describe "ResearchReports",:vcr, :mongoid do
 
+  it "should have link Research on homepage" do
+    visit root_path
+    find('#navigation-research_reports a').click
+    page.current_path.should eq research_reports_path
+  end
+
+  it "should have a link in Stock show page" do
+    create :'data_provider/company', :name => 'Petronet LNG Ltd.', :nse_code => nil, :bse_code1 => '532522'
+    visit stock_path(:id => 'petronet-lng-ltd')
+    within ".level_3" do
+      find('#navigation-research_reports a').click
+    end
+    page.current_path.should eq stock_research_reports_path(:stock_id => 'petronet-lng-ltd')
+  end
+
   it "should show latest reports in the index page" do
     visit research_reports_path
     page.should have_content "Emkay"
@@ -14,6 +29,21 @@ describe "ResearchReports",:vcr, :mongoid do
     click_on 'Search'
     tableish("table").should_not include (["Jun 27 2012", "Buy Petronet LNG; target of Rs 194", "Emkay", "Petronet LNG Ltd", "Oil and Gas"])
     tableish("table").should include (["May 28 2012", "Accumulate NMDC; target of Rs 187", "Angel Broking", "NMDC", "Mining"])
+  end
+
+  it "should show notice message when no search results" do
+    visit research_reports_path
+    fill_in 'query', :with => "Ged"
+    click_on 'Search'
+    page.should have_content I18n.t("empty_search")
+  end
+
+  it "should relevent message for search results" do
+    visit research_reports_path
+    fill_in 'query', :with => "Em"
+    click_on 'Search'
+    page.should have_content I18n.t("results_for_search", :term => 'Em')
+
   end
 
   it "should list all the reports for nse company" do
