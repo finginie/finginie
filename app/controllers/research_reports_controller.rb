f5f5fad219673d9946b43d9663879a7da1cf6648
Subhash Chandra <subhash@investopresto.com>
2012-07-09 14:13:20 +0530

    fixed up subnav for shares section

--- app/controllers/research_reports_controller.rb
+++ app/controllers/research_reports_controller.rb
@@ -2,6 +2,7 @@ class ResearchReportsController < InheritedResources::Base
   optional_belongs_to :stock, :parent_class => DataProvider::Company, :finder => :find_by_slug, :instance_name => 'company'
 
   def collection
+    @search = DataProvider::Company.new
     conditions = { :query => params[:query] }
     conditions.merge!({ :nse_code => parent.nse_code, :bse_code => parent.bse_code1 }) if parent
     @reports = ResearchReport.filter(conditions)
