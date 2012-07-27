08fa5396d3842bfae2ae63fcb97308b9f54781ca
sankaranarayanan <esankaranarayanan@gmail.com>
2012-07-27 20:33:43 +0530

    special offer flash message added

--- app/controllers/ideal_investments_controller.rb
+++ app/controllers/ideal_investments_controller.rb
@@ -1,6 +1,7 @@
 class IdealInvestmentsController < InheritedResources::Base
   defaults :singleton => true, :resource_class => IdealInvestmentMix,:instance_name => 'ideal_investment_mix'
-  before_filter :user_logged_in?
+  before_filter :user_logged_in
+
   def resource
     @resource = super
     @resource.initial_investment=(params[:initial_investment]) if params[:initial_investment]
@@ -14,7 +15,10 @@ protected
     @current_user.comprehensive_risk_profiler
   end
 
-  def user_logged_in?
-    redirect_to edit_comprehensive_risk_profiler_path unless (@current_user && @current_user.comprehensive_risk_profiler.persisted?)
+  def user_logged_in
+    unless @current_user && @current_user.comprehensive_risk_profiler.persisted?
+      flash.keep(:notice)
+      redirect_to edit_comprehensive_risk_profiler_path
+    end
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,12 @@ class SessionsController < ApplicationController
   def success
     current_user.merge_comprehensive_risk_profiler(session[:comprehensive_risk_profiler]) && session[:comprehensive_risk_profiler] = nil if session[:comprehensive_risk_profiler]
     @redirect_uri  = session[:user_return_to] || main_app.root_path
-    flash[:notice] = 'Successfully signed in'
+    flash[:notice] = special_offer?(@redirect_uri) ? I18n.t('.special_offer.message') : 'Successfully signed in'
     render :layout => false
+  end
+
+private
+  def special_offer?(redirect_uri)
+    redirect_uri.include?("special_offer=true")
   end
 end
