e08ff26f39f462891a96c004ad362a89016902e2
sankaranarayanan <esankaranarayanan@gmail.com>
2012-08-06 17:20:11 +0530

    Added facebook recommendation button

---
+++ app/helpers/social_network_helper.rb
@@ -0,0 +1,15 @@
+module SocialNetworkHelper
+  def facebook_recommendation(content, options = {})
+    content_tag :div do
+      html_content = content_tag(:h1, content.html_safe, options)
+      html_content += facebook_recommendation_tag
+    end
+  end
+
+  def facebook_recommendation_tag
+    tag :div, :class => 'fb-like', :data => { :action => "recommend", :font => "lucida grande",
+                                              :href => current_page_url,
+                                              :layout => "button_count", :send => "false",
+                                              :'show-faces' => "true", :width => "450" }
+  end
+end
