3ab0cbf30bb221564671d3590a2a4773f320be3f
sankaranarayanan <esankaranarayanan@gmail.com>
2012-08-10 20:51:25 +0530

    Fixed bugs in research report

--- app/models/data_provider/company.rb
+++ app/models/data_provider/company.rb
@@ -6,16 +6,22 @@ module DataProvider
     scope :nifty, where(:security_code.in => Listing.nifty.map(&:security_code))
 
     def all_brokerages_recommendation_values
-      report = ResearchReport.short_term(ticker_name)
+      report = ResearchReport.short_term(nse_code)
       report.map(&:recommendation_value)
     end
 
     def rating
-      all_brokerages_recommendation_values.size > 0 ? (all_brokerages_recommendation_values.sum.to_f / all_brokerages_recommendation_values.size).round : 0
+      return 0 if all_brokerages_recommendation_values.empty?
+
+      if brokerage_strong_recommendation_criteria?
+        5
+      else
+        (all_brokerages_recommendation_values.sum.to_f / all_brokerages_recommendation_values.size).round
+      end
     end
 
     def recommendation
-      if strong_brokerage_recommendation_criteria?
+      if brokerage_strong_recommendation_criteria?
         "Strong Buy"
       else
         ResearchReport::RECOMMENDATION[rating]
@@ -23,8 +29,8 @@ module DataProvider
     end
 
   private
-   def strong_brokerage_recommendation_criteria?
-    all_brokerages_recommendation_values.uniq.count == 1 && all_brokerages_recommendation_values.count(5) > 3
-   end
+    def brokerage_strong_recommendation_criteria?
+      all_brokerages_recommendation_values.uniq.count == 1 && all_brokerages_recommendation_values.count(5) > 3
+    end
   end
 end
