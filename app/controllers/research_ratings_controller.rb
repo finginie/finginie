82c066ca1550d9fee67c114f646479ff153a24c3
sankaranarayanan <esankaranarayanan@gmail.com>
2012-08-13 14:53:09 +0530

    moved sheetmapper to activerecord

--- app/controllers/research_ratings_controller.rb
+++ app/controllers/research_ratings_controller.rb
@@ -2,7 +2,7 @@ class ResearchRatingsController < InheritedResources::Base
   actions :index
 
   def collection
-    @companies ||= DataProvider::Company.nifty.sort_by(&:rating).reverse.take(10)
+    @companies ||= DataProvider::Company.nifty.sort_by(&:rating).reverse
     CompanyDecorator.decorate(@companies)
   end
 end
