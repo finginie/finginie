345214b15c3465df6b92a817577f7594f5fda625
code-R <vamsi@investopresto.com>
2012-08-23 10:14:10 +0530

    Fixed bug for referring himself and created completed_step.

--- app/models/point_tracker/referral_step.rb
+++ app/models/point_tracker/referral_step.rb
@@ -3,5 +3,14 @@ module PointTracker
     DESCRIPTION = 'Every sign up from shared link'
     ACTION_LINK = '/comprehensive_risk_profiler/ideal_investments'
     POINTS      = 200
+
+    def save(meta_data = {})
+      super unless referred_himself?(meta_data[:referred_user_id])
+    end
+
+    private
+    def referred_himself?(referred_user_id)
+      referred_user_id == user.id
+    end
   end
 end
