8fcc1a39cf96e166ece663e7b1528e5d9bfe946f
sankaranarayanan <esankaranarayanan@gmail.com>
2012-08-25 01:24:24 +0530

    Fixed email template and configuration errors

--- app/mailers/ebola_mailer.rb
+++ app/mailers/ebola_mailer.rb
@@ -1,12 +1,10 @@
 class EbolaMailer < ActionMailer::Base
   include Resque::Mailer
 
-  default from: 'register@finginie.com'
+  default from: "'Finginie' <register@finginie.com>"
 
   def welcome_email(invited_users, share_url, from_email_id)
-    @url  = share_url
-    @fom_email = from_email_id
-    invited_users = invited_users.join("; ")
-    mail(:to => invited_users, :subject => "Welcome to My Awesome Site")
+    @share_url  = share_url
+    mail(:to => invited_users, :subject => "#{from_email_id} wants you to try Finginie")
   end
 end
