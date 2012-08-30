3473a3fe8579343fe060fceb346a2ddd2626c789
sankaranarayanan <esankaranarayanan@gmail.com>
2012-08-30 12:33:12 +0530

    Moved action mailer to postage app

--- app/mailers/ebola_mailer.rb
+++ app/mailers/ebola_mailer.rb
@@ -1,10 +1,18 @@
-class EbolaMailer < ActionMailer::Base
+class EbolaMailer < PostageApp::Mailer
   include Resque::Mailer
 
   default from: "'Finginie' <register@finginie.com>"
 
   def welcome_email(invited_users, share_url, from_email_id)
     @share_url  = share_url
-    mail(:to => invited_users, :subject => "#{from_email_id} wants you to try Finginie")
+    @from_user = from_email_id.split("@")[0]
+
+    headers['Reply-To'] = "'Finginie' <register@finginie.com>"
+
+    mail(
+      :from => from_email_id,
+      :subject => "#{from_email_id} wants you to try Finginie",
+      :to => invited_users
+      )
   end
 end
