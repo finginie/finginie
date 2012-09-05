0a1948026bc0b57bb4db96591855898943c7b9c3
code-R <vamsi@investopresto.com>
2012-09-05 15:31:20 +0530

    refactoring ebola_helper module for fb share urls.

--- app/helpers/ebola_helper.rb
+++ app/helpers/ebola_helper.rb
@@ -1,51 +1,48 @@
 module EbolaHelper
+  STEPS = {
+    :public_portfolio         => PointTracker::SharePortfolioOnFbStep,
+    :public_financial_profile => PointTracker::ShareFinancialProfileOnFbStep
+  }.with_indifferent_access
+
   def tracked_facebook_share_url_for(type)
-    config_params_hsh = send("#{type}_fb_hsh")
+    config_params_hash  = query_object(type).marshal_dump
+    facebook_share_url = ENV['FACEBOOK_SHARE_DIALOG_URL'] + fb_share_query_params(config_params_hash)
+
     content_tag('span', :class => 'ga-track-event', :data => {:category => 'share', :action => 'facebook'}) do
-      link_to facebook_share_url(config_params_hsh) do
+      link_to facebook_share_url do
         image_tag 'facebook.png'
       end
     end
   end
 
   private
-  def facebook_share_url(params_hsh)
-    ENV['FACEBOOK_SHARE_DIALOG_URL'] + fb_share_query_params(params_hsh)
+  def query_object(type)
+    query_params              = OpenStruct.new
+    query_params.link         = send("#{type}_share_url")
+    query_params.name         = translated_name_for(type)
+    query_params.description  = translated_description_for(type)
+    query_params.redirect_uri = facebook_callback_url_with_step(STEPS[type])
+
+    query_params
   end
 
-  def public_portfolio_fb_hsh
-    name = translated_name_for('public_portfolio')
-    description = translated_description_for('public_portfolio')
-
-    {
-      :link => public_portfolio_url(resource),
-      :name => name,
-      :description => description,
-      :redirect_uri => facebook_callback_url_with_step(PointTracker::SharePortfolioOnFbStep)
-    }
+  def public_portfolio_share_url
+    public_portfolio_url(resource)
   end
 
-  def public_financial_profile_fb_hsh
-    name = translated_name_for('public_financial_profile')
-    description = translated_description_for('public_financial_profile')
-
-    {
-      :link => public_financial_profile_url(current_user),
-      :name => name,
-      :description => description,
-      :redirect_uri => facebook_callback_url_with_step(PointTracker::ShareFinancialProfileOnFbStep)
-    }
+  def public_financial_profile_share_url
+    public_financial_profile_url(current_user)
   end
 
   def facebook_callback_url_with_step(step)
     social_network_facebook_callback_url(:return_to => current_page_url, :step => step)
   end
 
-  def fb_share_query_params(params_hsh)
+  def fb_share_query_params(params_hash)
     {
       :app_id => ENV['FACEBOOK_KEY'],
       :picture => host + '/assets/logo.png'
-    }.merge(params_hsh).to_query
+    }.merge(params_hash).to_query
   end
 
   def translated_name_for(type)
