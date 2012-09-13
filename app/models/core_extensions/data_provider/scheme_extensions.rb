f167ed990d420c04b5593277c9df2cb0f9f4a534
code-R <vamsi@investopresto.com>
2012-09-13 15:22:18 +0530

    Cleaning up financial planner and adding asset methods to dataprovider core extensions.

--- app/models/core_extensions/data_provider/scheme_extensions.rb
+++ app/models/core_extensions/data_provider/scheme_extensions.rb
@@ -3,7 +3,56 @@ require 'kaminari/models/mongoid_extension'
 
 module SchemeExtensions
   extend ActiveSupport::Concern
+
+  TOP_GOLD_ETFS = [
+    'Goldman Sachs Gold Exchange Traded Scheme-Growth',
+    'SBI Gold Exchange Traded Scheme-Growth',
+    'Kotak Gold ETF-Growth'
+  ]
+  LARGE_CAP_INDICES = ["BSE 100 Index", "S&P CNX 500 Equity Index", "NSE Index", "NSE CNX 100"]
+  MID_CAP_INDICES   = ["CNX Midcap Index", "BSE Mid-Cap Index"]
+
+  included do
+    scope :top_gold_etfs, where(:name.in => TOP_GOLD_ETFS)
+    scope :viable_indices, lambda { |indices_array|
+      only(:code, :name, :plan_code, :minimum_investment_amount, :size, :entry_load, :exit_load, :prev3_year_comp_percent).
+      where(:bench_mark_index_name.in => indices_array).
+      where(:minimum_investment_amount.lte => 5000, :size.gte => 50)
+    }
+    scope :elss_funds, where(:class_code => 2119)
+    scope :order_by_prev3_year_comp_percent, order_by(:prev3_year_comp_percent => :desc)
+  end
+
   module ClassMethods
+    def top_mid_cap_stocks(limit)
+      mid_cap_schemes = get_indices_with_min_entry_and_exit_load(MID_CAP_INDICES).take(3 * limit)
+      distinct_schemes(mid_cap_schemes).take(limit)
+    end
+
+    def top_large_cap_stocks(limit)
+      goldman_sachs_nifty_etf = where(:security_code => "17024319.002066").first
+      large_cap_schemes = [
+        get_indices_with_min_entry_and_exit_load(LARGE_CAP_INDICES).take(3 * limit),
+        goldman_sachs_nifty_etf
+      ].flatten.compact.sort_by(&:prev3_year_comp_percent).reverse.take(3 * limit)
+
+      distinct_schemes(large_cap_schemes).take(limit)
+    end
+
+    def get_indices_with_min_entry_and_exit_load(indices)
+      viable_indices(indices).order_by_prev3_year_comp_percent.select{ |scheme| (scheme.entry_load.to_f + scheme.exit_load.to_f) <= 2 }
+    end
+
+    def distinct_schemes(schemes)
+      distinct_schemes = schemes.group_by(&:code).values
+      growth_plan_code, dividend_plan_code = 2066, 2067
+      distinct_schemes.inject([]) do |result, plans|
+        grow_plan_schemes     = plans.select{|scheme| scheme.plan_code == growth_plan_code}.first
+        dividend_plan_schemes = plans.select{|scheme| scheme.plan_code == dividend_plan_code}.first
+        result << (grow_plan_schemes || dividend_plan_schemes)
+      end
+    end
+
     def top_gainers(count)
       count ||= 5
       active.equity_funds.order([[ :percentage_change, :desc ]]).limit(count)
--- app/models/financial_planner.rb
+++ app/models/financial_planner.rb
@@ -1,28 +1,47 @@
 class FinancialPlanner
 
-  IDEAL_ASSET_ALLOCATION = {
-    1 => { 'Fixed Deposits' => 100,  'Large Cap Stocks' => 0,   'Mid Cap Stocks' => 0,  'Gold' => 0  },
+  attr_reader :score
 
-    2 => { 'Fixed Deposits' => 100,  'Large Cap Stocks' => 0,   'Mid Cap Stocks' => 0,  'Gold' => 0  },
+  IDEAL_ASSET_ALLOCATION_CONFIG = [
+    { 'Fixed Deposits' => 100,  'Large Cap Stocks' => 0,   'Mid Cap Stocks' => 0,  'Gold' => 0  }, #score - 1
 
-    3 => { 'Fixed Deposits' => 80,   'Large Cap Stocks' => 0,   'Mid Cap Stocks' => 0,  'Gold' => 20 },
+    { 'Fixed Deposits' => 100,  'Large Cap Stocks' => 0,   'Mid Cap Stocks' => 0,  'Gold' => 0  }, #score - 2
 
-    4 => { 'Fixed Deposits' => 60,   'Large Cap Stocks' => 15,  'Mid Cap Stocks' => 0,  'Gold' => 25 },
+    { 'Fixed Deposits' => 80,   'Large Cap Stocks' => 0,   'Mid Cap Stocks' => 0,  'Gold' => 20 }, #score - 3
 
-    5 => { 'Fixed Deposits' => 50,   'Large Cap Stocks' => 25,  'Mid Cap Stocks' => 0,  'Gold' => 25 },
+    { 'Fixed Deposits' => 60,   'Large Cap Stocks' => 15,  'Mid Cap Stocks' => 0,  'Gold' => 25 }, #score - 4
 
-    6 => { 'Fixed Deposits' => 40,   'Large Cap Stocks' => 20,  'Mid Cap Stocks' => 10, 'Gold' => 30 },
+    { 'Fixed Deposits' => 50,   'Large Cap Stocks' => 25,  'Mid Cap Stocks' => 0,  'Gold' => 25 }, #score - 5
 
-    7 => { 'Fixed Deposits' => 35,   'Large Cap Stocks' => 20,  'Mid Cap Stocks' => 15, 'Gold' => 30 },
+    { 'Fixed Deposits' => 40,   'Large Cap Stocks' => 20,  'Mid Cap Stocks' => 10, 'Gold' => 30 }, #score - 6
 
-    8 => { 'Fixed Deposits' => 30,   'Large Cap Stocks' => 20,  'Mid Cap Stocks' => 20, 'Gold' => 30 },
+    { 'Fixed Deposits' => 35,   'Large Cap Stocks' => 20,  'Mid Cap Stocks' => 15, 'Gold' => 30 }, #score - 7
 
-    9 => { 'Fixed Deposits' => 20,   'Large Cap Stocks' => 20,  'Mid Cap Stocks' => 30, 'Gold' => 30 },
+    { 'Fixed Deposits' => 30,   'Large Cap Stocks' => 20,  'Mid Cap Stocks' => 20, 'Gold' => 30 }, #score - 8
 
-    10 => {'Fixed Deposits' => 10,   'Large Cap Stocks' => 15,  'Mid Cap Stocks' => 45, 'Gold' => 30 }
-  }
+    { 'Fixed Deposits' => 20,   'Large Cap Stocks' => 20,  'Mid Cap Stocks' => 30, 'Gold' => 30 }, #score - 9
 
-  def self.ideal_asset_allocation(comprehensive_risk_profiler)
-    IDEAL_ASSET_ALLOCATION[comprehensive_risk_profiler.score.round] if comprehensive_risk_profiler.score
+    {'Fixed Deposits' => 10,   'Large Cap Stocks' => 15,  'Mid Cap Stocks' => 45, 'Gold' => 30 }   #score - 10
+  ]
+
+  ASSET_CLASSES = %w(fixed_deposits large_cap_stocks mid_cap_stocks gold)
+
+  def initialize(score)
+    @score = score.round
   end
+
+  ASSET_CLASSES.each do |asset_class|
+    method = "#{asset_class}_percent"                      ##
+    define_method(method) do                               # def fixed_depoists_percent
+      ideal_asset_allocation[asset_class.titleize]         #   ideal_asset_allocation['Fixed Deposits']
+    end                                                    # end
+  end                                                      ##
+
+  def ideal_asset_allocation
+    @ideal_asset_allocation ||= IDEAL_ASSET_ALLOCATION_CONFIG[score - 1]
+  end
+
+  # def self.ideal_asset_allocation(comprehensive_risk_profiler)
+  #   IDEAL_ASSET_ALLOCATION[comprehensive_risk_profiler.score.round] if comprehensive_risk_profiler.score
+  # end
 end
