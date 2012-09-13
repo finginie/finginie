4d912383e99e82983bbb9c0060c262d110254e91
Subhash Chandra <subhash@investopresto.com>
2012-09-13 14:24:56 +0530

    changed navigation links for user nav

--- app/helpers/chart_helper.rb
+++ app/helpers/chart_helper.rb
@@ -3,9 +3,7 @@ module ChartHelper
     capture_haml options do |options|
       if options[:data] && !options[:data].empty?
         elements = GoogleChart.new(:data => options.delete(:data), :header => options.delete(:header)).elements
-        haml_tag('.chart', :data => { :elements => elements}.merge(options), :id => options[:title].parameterize) do
-          haml_concat 'Loading'
-        end
+        haml_tag('.chart', :data => { :elements => elements}.merge(options), :id => options[:title].parameterize)
       else
         haml_tag :div do
            haml_tag :span, I18n.t('.data_not_available')
