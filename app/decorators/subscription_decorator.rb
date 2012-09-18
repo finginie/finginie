588d9e4adada5126dd0299339106d02d019e0901
Subhash Chandra <subhash@investopresto.com>
2012-09-18 10:00:03 +0530

    refactored subscriptions decorator

--- app/decorators/subscription_decorator.rb
+++ app/decorators/subscription_decorator.rb
@@ -2,29 +2,25 @@ class SubscriptionDecorator < ApplicationDecorator
   decorates :subscription
 
   def render_user(size = :small)
-    render_person user, size if user
+    return unless user
+    render_subscribable ProfileDecorator.decorate(user), size
   end
 
   def render_profile(size = :small)
     return unless subscribable_type == 'User'
-    render_person subscribable, size
+    render_subscribable ProfileDecorator.decorate(subscribable), size
   end
 
   def render_portfolio(size = :small)
     return unless subscribable_type == 'Portfolio'
-    portfolio = PublicPortfolioDecorator.decorate subscribable
-    image = h.content_tag :div, portfolio.avatar(size), :class => :span2
-    name = h.content_tag :div, portfolio.link, :class => :span6
-    unfollow_button = h.content_tag :div, portfolio.follow_unfollow_button(size), :class => %w(span4 align-right)
-    h.content_tag :div, image + name + unfollow_button, :class => 'row-fluid'
+    render_subscribable PublicPortfolioDecorator.decorate(subscribable), size
   end
 
 private
-  def render_person(person, size = :small)
-    person = ProfileDecorator.decorate person
-    image = h.content_tag :div, person.avatar(size), :class => :span2
-    name = h.content_tag :div, person.link, :class => :span6
-    unfollow_button = h.content_tag :div, person.follow_unfollow_button(size), :class => %w(span4 align-right)
+  def render_subscribable(subscribable, size = :small)
+    image = h.content_tag :div, subscribable.avatar(size), :class => :span2
+    name = h.content_tag :div, subscribable.link, :class => :span6
+    unfollow_button = h.content_tag :div, subscribable.follow_unfollow_button(size), :class => %w(span4 align-right)
     h.content_tag :div, image + name + unfollow_button, :class => 'row-fluid'
   end
 end
